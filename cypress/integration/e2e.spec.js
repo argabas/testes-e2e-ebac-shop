f0f2c72e47b83b4c4942088b732546ca84217a0d
argabas <argabas@outlook.com>
2022-04-04 17:22:36 -0300

    base

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -10,12 +10,26 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('produtos')
     });
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
-    });
+    it('Adicionar produtos ao carrinho', () => {
+        cy.addProdutos('Ariel Roll Sleeve Sweatshirt', 1, 'L', 'Green')
+        cy.addProdutos('Abominable Hoodie', 1, 'L', 'Blue')
+        cy.addProdutos('Aero Daily Fitness Tee', 1, 'XS', 'Black')
+        cy.addProdutos('Ajax Full-Zip Sweatshirt', 1, 'M', 'Red')
+        
+        cy.get('#cart > .dropdown-toggle').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+        cy.get('.showlogin').click()
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-button').click()
+        cy.get('#payment_method_cod').click()
+        //cy.get('.woocommerce form > .form-row > .input-checkbox').click()
+        cy.get('#place_order').click()
+        cy.get('.woocommerce-notice').should('contain', "Obrigado. Seu pedido foi recebido.")
 
+    });
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,14 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('addProdutos', (produto, quantidade, tamanho, cor) => {
+    cy.get('[class="product-block grid"]')
+        .contains(produto)
+        .click()
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-' + cor).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+    cy.visit('produtos')
+})
+
