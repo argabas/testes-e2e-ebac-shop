4e854a51742a1e8125f400c8b82a1535423a58e2
argabas <argabas@outlook.com>
2022-04-05 09:21:55 -0300

    Teste valido sem melhoria

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -26,7 +26,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-button').click()
         cy.get('#payment_method_cod').click()
-        //cy.get('.woocommerce form > .form-row > .input-checkbox').click()
+        cy.get('#terms').check({force:true})
         cy.get('#place_order').click()
         cy.get('.woocommerce-notice').should('contain', "Obrigado. Seu pedido foi recebido.")
 
